b42705fe7b9de56afc96c3882d9d85ea0aa6865a
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-08-02 15:23:43 -0700

    Updated documentation

--- src/Indices.hs
+++ src/Indices.hs
@@ -1,28 +1,28 @@
 module Indices
     (
-      -- * Types
+      -- * Primary Index
       PrimaryIndex
-    , SecondaryIndex
-
-      -- * Indexing
     , primaryIndex
-    , secondaryIndex
-
-      -- * Querying
     , queryPrimary
+
+      -- * Secondary Index
+    , secondaryIndex
+    , SecondaryIndex
     , querySecondary
     ) where
-
 
 import Atom (Atom, atomToPage)
 import Chemistry (ParseS)
 import Control.Arrow (second, (&&&))
+import Control.Error (maximumDef)
 import Control.Monad (forM_)
 import Control.Monad.Trans.Class (lift)
 import Correspond (match)
 import qualified Data.ByteString as B
 import Control.DeepSeq (NFData)
 import Data.List (isSuffixOf, nub, sort)
+import Data.Monoid (Monoid(mempty))
+import qualified Data.HashMap.Strict as H
 import qualified Data.Set as S
 import qualified Data.Vector as V
 import qualified Data.Vector.Storable as VS
@@ -38,89 +38,6 @@ import Structure (atomsToStructure)
 import System.Directory (getDirectoryContents)
 import System.FilePath.Posix ((</>))
 import Util (groupOn)
-
-import Control.Arrow (second)
-import Control.Error (maximumDef)
-import Control.DeepSeq (NFData)
-import qualified Data.HashMap.Strict as H
-import Data.Monoid (Monoid(mempty))
-import qualified Data.Set as S
-import qualified Data.Vector as V
-import HSerialize (HSerialize(get, put))
-
-newtype SecondaryIndex = SecondaryIndex
-    { unSecondaryIndex
-        :: V.Vector (PDBID, VS.Vector Atom, V.Vector (V.Vector (VS.Vector Int)))
-    }
-
-instance NFData SecondaryIndex
-
-instance HSerialize SecondaryIndex where
-    get = fmap SecondaryIndex get
-    put = put . unSecondaryIndex
-
-filesToPageServer
- :: Size -> FilePath -> Producer (PDBID, [Atom]) IO ()
-filesToPageServer size pdbDir = do
-    files <- lift $ getDirectoryContents pdbDir
-    let pdbFiles = filter (isSuffixOf ".pdb") files
-    forM_ pdbFiles $ \pdbFile -> do
-        str <- lift $ B.readFile (pdbDir </> pdbFile)
-        let pdbID       = take 4 pdbFile
-            allAtoms    = pdbToAtoms str
-            atomsByPage = groupOn (atomToPage size) allAtoms
-        forM_ atomsByPage $ \atoms -> yield (pdbID, atoms)
-
-secondaryIndex
- :: Size
- -> FilePath
- -> MotifGraphs
- -> IO SecondaryIndex
-secondaryIndex size pdbDir parsers = do
-    v <- runVector $ run $
-         hoist lift (   filesToPageServer size pdbDir
-                    >-> progress
-                    >-> (for cat (yield . f . second (tokenize parsers)))
-                    )
-                    >-> foldVector
-    putStrLn ""
-    return (SecondaryIndex v)
-  where
-    f (a, (b, c)) = (a, b, c)
-
-tokenize
-   :: MotifGraphs
-   -> [Atom]
-   -> (VS.Vector Atom, V.Vector (V.Vector (VS.Vector Int)))
-tokenize parsers
-  = (id &&& motifsInStructure parsers . atomsToStructure) . VS.fromList
-
-querySecondary
-    :: MotifGraphs
-    -> PrimaryIndex
-    -> SecondaryIndex
-    -> Maybe Seed
-    -> [Atom]
-    -> ([Atom], [(PDBID, [Atom], [Atom])])
-querySecondary parsers i1 (SecondaryIndex i2) mSeed atoms = do
-    let (queryAtoms, queryMotifs) = tokenize parsers (meld atoms)
-        querySubset = map (queryAtoms VS.!) . sort . nub $ do
-            motif     <- V.toList queryMotifs
-            incidence <- V.toList motif
-            VS.toList incidence
-        candidates = case (queryPrimary i1 queryMotifs) of
-            Nothing    -> []
-            Just pages -> do
-                page <-
-                    (case mSeed of
-                        Nothing   -> id
-                        Just seed -> shuffle seed (S.size pages)
-                    ) (S.toList pages)
-                let (pdbID, pageAtoms, pageMotifs) = i2 V.! page
-                    context = VS.toList pageAtoms
-                map (\indices -> (pdbID, map (pageAtoms VS.!) indices, context))
-                  $ match queryMotifs pageMotifs
-     in (querySubset, candidates)
 
 newtype PrimaryIndex = PrimaryIndex
     { unPrimaryIndex :: V.Vector (V.Vector (S.Set Int)) }
@@ -187,3 +104,81 @@ queryPrimary (PrimaryIndex i1) iq
                $ incidences)
         i1
         (V.map V.length iq)
+
+-- | Detailed index
+newtype SecondaryIndex = SecondaryIndex
+    { unSecondaryIndex
+        :: V.Vector (PDBID, VS.Vector Atom, V.Vector (V.Vector (VS.Vector Int)))
+    }
+
+instance NFData SecondaryIndex
+
+instance HSerialize SecondaryIndex where
+    get = fmap SecondaryIndex get
+    put = put . unSecondaryIndex
+
+filesToPageServer
+ :: Size -> FilePath -> Producer (PDBID, [Atom]) IO ()
+filesToPageServer size pdbDir = do
+    files <- lift $ getDirectoryContents pdbDir
+    let pdbFiles = filter (isSuffixOf ".pdb") files
+    forM_ pdbFiles $ \pdbFile -> do
+        str <- lift $ B.readFile (pdbDir </> pdbFile)
+        let pdbID       = take 4 pdbFile
+            allAtoms    = pdbToAtoms str
+            atomsByPage = groupOn (atomToPage size) allAtoms
+        forM_ atomsByPage $ \atoms -> yield (pdbID, atoms)
+
+{-| Given a page 'Size' a PDB file directory, and a set of parsed motifs,
+    compute the 'SecondaryIndex'
+-}
+secondaryIndex
+ :: Size
+ -> FilePath
+ -> MotifGraphs
+ -> IO SecondaryIndex
+secondaryIndex size pdbDir parsers = do
+    v <- runVector $ run $
+         hoist lift (   filesToPageServer size pdbDir
+                    >-> progress
+                    >-> (for cat (yield . f . second (tokenize parsers)))
+                    )
+                    >-> foldVector
+    putStrLn ""
+    return (SecondaryIndex v)
+  where
+    f (a, (b, c)) = (a, b, c)
+
+tokenize
+   :: MotifGraphs
+   -> [Atom]
+   -> (VS.Vector Atom, V.Vector (V.Vector (VS.Vector Int)))
+tokenize parsers
+  = (id &&& motifsInStructure parsers . atomsToStructure) . VS.fromList
+
+querySecondary
+    :: MotifGraphs
+    -> PrimaryIndex
+    -> SecondaryIndex
+    -> Maybe Seed
+    -> [Atom]
+    -> ([Atom], [(PDBID, [Atom], [Atom])])
+querySecondary parsers i1 (SecondaryIndex i2) mSeed atoms = do
+    let (queryAtoms, queryMotifs) = tokenize parsers (meld atoms)
+        querySubset = map (queryAtoms VS.!) . sort . nub $ do
+            motif     <- V.toList queryMotifs
+            incidence <- V.toList motif
+            VS.toList incidence
+        candidates = case (queryPrimary i1 queryMotifs) of
+            Nothing    -> []
+            Just pages -> do
+                page <-
+                    (case mSeed of
+                        Nothing   -> id
+                        Just seed -> shuffle seed (S.size pages)
+                    ) (S.toList pages)
+                let (pdbID, pageAtoms, pageMotifs) = i2 V.! page
+                    context = VS.toList pageAtoms
+                map (\indices -> (pdbID, map (pageAtoms VS.!) indices, context))
+                  $ match queryMotifs pageMotifs
+     in (querySubset, candidates)
