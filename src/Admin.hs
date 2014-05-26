b648eb4765600e6934cea76cad9bf2bac9e16c14
Gabriel Gonzalez <Gabriel439@gmail.com>
2014-05-25 19:53:07 -0700

    Polishing up Admin.hs

--- src/Admin.hs
+++ src/Admin.hs
@@ -25,26 +25,20 @@ module Main (
     main  
     ) where
 
-import qualified AMQP.Error as AE
 import AMQP.Types (HostName, QueueName)
 import Control.Applicative ((<$>), (<*>))
 import Control.Exception (bracket)
-import Control.Error (scriptIO, runScript, (!?))
-import qualified Data.Map as M
 import Data.Monoid (mconcat, (<>))
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import Log (initLog)
+import Data.Text (pack)
 import qualified Network.AMQP as A
-import qualified Network.AMQP.Types as A
 import qualified Options.Applicative as O
-import Password (Password, getPassword, password)
-import System.Environment (getArgs, getProgName)
 
-data Options = Options { hostName :: HostName , queue :: QueueName }
+import qualified AMQP.Error as AE
+import Log (initLog)
+import Password (getPassword, password)
 
-options :: O.Parser Options
-options = Options
+options :: O.Parser (HostName, QueueName)
+options = (,)
     <$> (O.strOption $ mconcat [
         O.short 'n',
         O.long "node",
@@ -53,7 +47,7 @@ options = Options
         O.showDefaultWith id,
         O.completer (O.bashCompleter "hostname"),
         O.help "AMQP node to connect to" ] )
-    <*> (fmap T.pack $ O.strOption $ mconcat [
+    <*> (fmap pack $ O.strOption $ mconcat [
         O.short 'q',
         O.long "queue",
         O.metavar "QUEUE",
@@ -61,7 +55,7 @@ options = Options
         O.showDefaultWith id,
         O.help "Queue to initialize" ] )
 
-parserInfo :: O.ParserInfo Options
+parserInfo :: O.ParserInfo (HostName, QueueName)
 parserInfo = O.info (O.helper <*> options) $ mconcat
     [ O.fullDesc
     , O.header "The Suns setup utility"
@@ -73,14 +67,14 @@ parserInfo = O.info (O.helper <*> options) $ mconcat
     and queues
 -}
 main :: IO ()
-main = runScript $ do
-    Options hostName queue <- scriptIO $ O.execParser parserInfo
+main = do
+    (hostName, queue) <- O.execParser parserInfo
 
-    scriptIO initLog
+    initLog
 
-    pwd <- scriptIO password
+    pwd <- password
 
-    scriptIO $ bracket
+    bracket
         (A.openConnection hostName "suns-vhost" "suns-admin" (getPassword pwd))
         A.closeConnection
         $ \connection -> do
@@ -107,12 +101,12 @@ main = runScript $ do
                 }
         
             let qName = "suns-queue-" <> queue
-            AE.declareQueue channel $ A.QueueOpts
-                qName                  -- queueName
-                False                  -- queuePassive
-                True                   -- queueDurable
-                False                  -- queueExclusive
-                False                  -- queueAutoDelete
-                (A.FieldTable M.empty) -- queueHeaders
+            AE.declareQueue channel $ A.newQueue
+                { A.queueName       = qName
+                , A.queuePassive    = False
+                , A.queueDurable    = True
+                , A.queueExclusive  = False
+                , A.queueAutoDelete = False
+                }
         
             AE.bindQueue channel qName xName1 queue
